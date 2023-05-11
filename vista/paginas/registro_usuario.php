f63bd909c917d4c5f09773528229f7feb5e34edf
Christian Vega <hasergecris9@gmail.com>
2023-05-11 15:36:16 -0500

    actualizacion

--- vista/paginas/registro_usuario.php
+++ vista/paginas/registro_usuario.php
@@ -1,17 +1,60 @@
-<?php 
-  require("dashboard.php")
+<?php
+require("dashboard.php")
 ?>
 
 <script>
+  // Obtener elementos del DOM
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('canvas');
+  const photo = document.getElementById('photo');
+  const captureButton = document.getElementById('capture');
+
+  // Obtener acceso a la cámara
+  navigator.mediaDevices.getUserMedia({
+      video: true
+    })
+    .then(stream => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(err => {
+      console.error('Error al obtener acceso a la cámara', err);
+    });
+
+  // Capturar foto
+  captureButton.addEventListener('click', () => {
+    // Mostrar la cámara y ocultar el botón de captura
+    video.style.display = 'block';
+    captureButton.style.display = 'none';
+
+    // Pintar la imagen de la cámara en el canvas
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // Mostrar la foto capturada
+    const dataURL = canvas.toDataURL();
+    photo.setAttribute('src', dataURL);
+    canvas.style.display = 'none';
+    photo.style.display = 'block';
+  });
+
+  // Mostrar el botón de captura al cargar la página
+  window.addEventListener('load', () => {
+    video.style.display = 'none';
+    canvas.style.display = 'block';
+    photo.style.display = 'none';
+    captureButton.style.display = 'flex';
+  });
+
   function cargarRol(rol) {
-      if( rol == 1 ){
-        document.getElementById("admin_contrasenia").style.display = "block";
-      } else {
-        document.getElementById("admin_contrasenia").style.display = "none";
-      }
-    }             
+    if (rol == 1) {
+      document.getElementById("admin_contrasenia").style.display = "block";
+    } else {
+      document.getElementById("admin_contrasenia").style.display = "none";
+    }
+  }
 </script>
-  
+
 
 
 <div id="ingreso_cliente">
@@ -29,18 +72,24 @@
                   <label for="nombres" class="form-label texto">Nombre Completo:</label>
                   <input type="text" class="form-control" id="nombres" name="registroNombre">
                 </div>
-                <div class=" col-md-4 caja-contador d-flex justify-content-end">
-                  <div class="foto  d-flex justify-content-center align-items-center">
-                  <!-- <input type="file" class="input_foto d-none"> -->
-                  <i class="fas fa-user icono_usuario"></i>
+                <div class="col-md-4 caja-contador d-flex justify-content-end">
+                  <div class="foto d-flex justify-content-center align-items-center">
+                    <button id="capture" class="boton-captura">
+                      <i class="fas fa-user icono-usuario"></i>
+                    </button>
+                    <video id="video" class="video"></video>
+                    <canvas id="canvas" class="canvas"></canvas>
+                    <img id="photo" class="photo" alt="Foto">
                   </div>
+
                 </div>
               </div>
-             
-                      
+
+
+
               <div class="col-md-12">
                 <label for="num_documento" class="form-label texto">Numero de documento:</label>
-                <input type="text" class="form-control" id="num_documento" name="registroDocumento"  aria-describedby="inputGroupPrepend2">
+                <input type="text" class="form-control" id="num_documento" name="registroDocumento" aria-describedby="inputGroupPrepend2">
               </div>
 
               <div class="col-md-12">
@@ -52,8 +101,8 @@
                 <label for="validacion_rol" class="form-label texto">Rol</label>
                 <select class="form-select" name="validacion_rol" id="validacion_rol" onchange="cargarRol(this.value);">
                   <option selected value="">Seleccionar.... </option>
-                  <option  value="1">Administrador</option>
-                  <option  value="2">Cliente</option>
+                  <option value="1">Administrador</option>
+                  <option value="2">Cliente</option>
                 </select>
               </div>
 
@@ -62,26 +111,25 @@
                 <input type="pasword" class="form-control" id="contrasenia" name="contrasenia">
               </div>
 
-              
+
               <?php
 
-// METODO ESTATICO
-                $registro = ControladorFormularios::ctrRegistroUsuarios();
+              // METODO ESTATICO
+              $registro = ControladorFormularios::ctrRegistroUsuarios();
 
-                if($registro == "ok") {
+              if ($registro == "ok") {
 
-                  echo 
-                  '<script>
+                echo
+                '<script>
                       if(window.history.replaceState) {
                         
                         window.history.replaceState(null,null,window.location.href);
                       }
                     </script>';
-                      
-                    echo '<div class="alert alert-success">El usuario ha sido registrado</div>';
-  
-                  }
-                ?>
+
+                echo '<div class="alert alert-success">El usuario ha sido registrado</div>';
+              }
+              ?>
 
               <div class="col-12 d-flex justify-content-end">
                 <button type="submit" class="btn btn-lg btn-primary boton_general">Enviar</button>
@@ -93,11 +141,3 @@
     </div>
   </div>
 </div>
-
-
-
-
-
-              
-
-
