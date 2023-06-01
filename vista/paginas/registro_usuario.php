c66ae3ace403616d4f59114b2beba089a20392d3
Christian Vega <hasergecris9@gmail.com>
2023-06-01 16:49:39 -0500

    actualizacion en registro usuario

--- vista/paginas/registro_usuario.php
+++ vista/paginas/registro_usuario.php
@@ -2,58 +2,6 @@
 require("dashboard.php")
 ?>
 
-<script>
-  // Obtener elementos del DOM
-  const video = document.getElementById('video');
-  const canvas = document.getElementById('canvas');
-  const photo = document.getElementById('photo');
-  const captureButton = document.getElementById('capture');
-
-  // Obtener acceso a la cámara
-  navigator.mediaDevices.getUserMedia({
-      video: true
-    })
-    .then(stream => {
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch(err => {
-      console.error('Error al obtener acceso a la cámara', err);
-    });
-
-  // Capturar foto
-  captureButton.addEventListener('click', () => {
-    // Mostrar la cámara y ocultar el botón de captura
-    video.style.display = 'block';
-    captureButton.style.display = 'none';
-
-    // Pintar la imagen de la cámara en el canvas
-    const context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-    // Mostrar la foto capturada
-    const dataURL = canvas.toDataURL();
-    photo.setAttribute('src', dataURL);
-    canvas.style.display = 'none';
-    photo.style.display = 'block';
-  });
-
-  // Mostrar el botón de captura al cargar la página
-  window.addEventListener('load', () => {
-    video.style.display = 'none';
-    canvas.style.display = 'block';
-    photo.style.display = 'none';
-    captureButton.style.display = 'flex';
-  });
-
-  function cargarRol(rol) {
-    if (rol == 1) {
-      document.getElementById("admin_contrasenia").style.display = "block";
-    } else {
-      document.getElementById("admin_contrasenia").style.display = "none";
-    }
-  }
-</script>
 
 
 
@@ -67,46 +15,32 @@ require("dashboard.php")
           </div>
           <div class="card-body">
             <form method="post" class="row">
-              <div class="col-12 d-flex justify-content-between align-items-center">
-                <div class="col-md-8">
-                  <label for="nombres" class="form-label texto">Nombre Completo:</label>
-                  <input type="text" class="form-control" id="nombres" name="registroNombre">
-                </div>
-                <div class="col-md-4 caja-contador d-flex justify-content-end">
-                  <div class="foto d-flex justify-content-center align-items-center">
-                    <button id="capture" class="boton-captura">
-                      <i class="fas fa-user icono-usuario"></i>
-                    </button>
-                    <video id="video" class="video"></video>
-                    <canvas id="canvas" class="canvas"></canvas>
-                    <img id="photo" class="photo" alt="Foto">
-                  </div>
 
-                </div>
+              <div class="col-md-12 mb-3">
+                <label for="nombres" class="form-label texto">Nombre Completo:</label>
+                <input type="text" class="form-control" id="nombres" name="registroNombre" required>
               </div>
 
-
-
-              <div class="col-md-12">
+              <div class="col-md-12 mb-3">
                 <label for="num_documento" class="form-label texto">Numero de documento:</label>
-                <input type="text" class="form-control" id="num_documento" name="registroDocumento" aria-describedby="inputGroupPrepend2">
+                <input type="text" class="form-control" id="num_documento" name="registroDocumento" aria-describedby="inputGroupPrepend2" required>
               </div>
 
-              <div class="col-md-12">
-                <label for=correo" class="form-label texto">Correo:</label>
-                <input type="email" class="form-control" id="correo" name="registroCorreo">
+              <div class="col-md-12 mb-3">
+                <label for=correo class="form-label texto">Correo:</label>
+                <input type="email" class="form-control" id="correo" name="registroCorreo" required>
               </div>
 
-              <div class="col-md-6">
+              <div class="col-md-6 mb-3">
                 <label for="validacion_rol" class="form-label texto">Rol</label>
-                <select class="form-select" name="validacion_rol" id="validacion_rol" onchange="cargarRol(this.value);">
+                <select class="form-select" name="validacion_rol" id="validacion_rol" onchange="cargarRol(this.value);" required>
                   <option selected value="">Seleccionar.... </option>
                   <option value="1">Administrador</option>
                   <option value="2">Cliente</option>
                 </select>
               </div>
 
-              <div class="col-md-6 mb-4" id="admin_contrasenia">
+              <div class="col-md-6 mb-3" id="admin_contrasenia">
                 <label for="contrasenia" class="form-label texto">Contraseña:</label>
                 <input type="password" class="form-control" id="contrasenia" name="contrasenia">
               </div>
@@ -121,19 +55,19 @@ require("dashboard.php")
 
                 echo
                 '<script>
-                      if(window.history.replaceState) {
-                        
-                        window.history.replaceState(null,null,window.location.href);
-                      }
-                    </script>';
+                        if(window.history.replaceState) {
+                          
+                          window.history.replaceState(null,null,window.location.href);
+                        }
+                      </script>';
 
                 echo '<div class="alert alert-success">El usuario ha sido registrado</div>';
 
                 '<script>
-                    setTimeout(fuction(){
-                      window.location = "index.php?pagina=lista_usuario";
-                    },3000);
-                  </script>';
+                      setTimeout(fuction(){
+                        window.location = "index.php?pagina=lista_usuario";
+                      },3000);
+                    </script>';
               }
               ?>
 
